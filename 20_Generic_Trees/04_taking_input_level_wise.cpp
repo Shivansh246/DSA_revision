472c75d82368d075dfa3b7871b136af5caa2f6cc
Shivansh246 <285635402+Shivansh246@users.noreply.github.com>
2026-07-01 11:49:48 +0530

    take level wise input using stack

---
+++ 20_Generic_Trees/04_taking_input_level_wise.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+template<typename T>
+class TreeNode{
+    public:
+    T data;
+    vector<TreeNode<T>*> children;
+    TreeNode(T data){
+        this->data = data;
+    }
+};
+TreeNode<int> *takeInput(){
+    int rootdata;
+    cout<<"Enter root data: ";
+    cin>>rootdata;
+    TreeNode<int>*root = new TreeNode<int>(rootdata);
+    queue<TreeNode<int> *> q;
+    q.push(root);
+
+    while(!q.empty()){
+        TreeNode<int> *f=q.front();
+        q.pop();
+        int n;
+        cout<<"Enter no of nodes for node "<<f->data<<": ";
+        cin>>n;
+        for(int i=1;i<=n;i++){
+            int childData;
+            cout<<"Enter data for "<<i<<"th"<<" node of "<<f->data<<": ";
+            cin>>childData;
+            TreeNode<int> *childNode = new TreeNode<int>(childData);
+            q.push(childNode);
+            f->children.push_back(childNode);
+        }
+    }
+    return root;
+}
+void printTree(TreeNode<int> *root){
+   if(root==NULL){
+    return;
+   }
+    cout<<root->data<<": ";
+    for(int i=0;i<root->children.size();i++){
+        cout<<root->children[i]->data<<", ";
+    }
+    cout<<endl;
+    for(int i=0;i<root->children.size();i++){ 
+        printTree(root->children[i]);
+    }
+
+}
+int main(){
+    TreeNode<int> *root = takeInput();
+    printTree(root);
+    return 0;
+}
