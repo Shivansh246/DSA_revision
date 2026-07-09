70ac10c5bd825a6a687c6f0ed311c126f2c71049
Shivansh246 <285635402+Shivansh246@users.noreply.github.com>
2026-07-09 06:50:25 +0530

    find a node, find min and max node in a binary tree

---
+++ 21_Binary_Tree/14_find_max.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class BTnode{
+    public:
+    T data;
+    BTnode *left;
+    BTnode *right;
+    BTnode(T data){
+        this->data=data;
+        left = NULL;
+        right = NULL;
+    }
+    ~BTnode(){//calls recursively like the destructor of general tree
+        delete left;
+        delete right;
+    }
+};
+
+void printTree(BTnode<int> *root){
+    if(root == NULL){
+        return;
+    }
+    cout<<root->data<<": ";
+    if(root->left!=NULL){
+        cout<<"L"<<root->left->data<<" ";
+    }
+    if(root->right!=NULL){
+        cout<<"R"<<root->right->data;
+    }
+    cout<<endl;
+    printTree(root->left);
+    printTree(root->right);
+}
+BTnode<int> *takeInput(){
+    int rootdata;
+    cout<<"Enter root data: ";
+    cin>>rootdata;
+    if(rootdata==-1){
+        return NULL;
+    }
+    BTnode<int> *root = new BTnode<int>(rootdata);
+    queue<BTnode<int>*> q;
+    q.push(root);
+    while(!q.empty()){
+        BTnode<int> *f = q.front();
+        q.pop();
+        int leftdata;
+        cout<<"Enter left data of "<<f->data<<": ";
+        cin>>leftdata;
+        if(leftdata!=-1){
+            BTnode<int> *leftnode = new BTnode<int>(leftdata);
+            q.push(leftnode);
+            f->left = leftnode;
+        }
+        int rightdata;
+        cout<<"Enter right data of "<<f->data<<": ";
+        cin>>rightdata;
+        if(rightdata!=-1){
+            BTnode<int> *rightnode = new BTnode<int>(rightdata);
+            q.push(rightnode);
+            f->right = rightnode;
+        }
+    }
+    return root;
+}
+int findMax(BTnode<int> *root){
+    if(root==NULL){
+        return 0;
+    }
+    int ans=root->data;
+    int leftmax= findMax(root->left);
+    int rightmax = findMax(root->right);
+    return max(ans,max(rightmax,leftmax));
+}
+void findMaxOtherWay(BTnode<int> *root,int &ans){
+    if(root==NULL){
+        return ;
+    }
+    ans  = max(ans,root->data);
+    findMaxOtherWay(root->left,ans);
+    findMaxOtherWay(root->right,ans);
+}
+int main(){
+    BTnode<int> *root =takeInput();
+    printTree(root);
+    int ans = INT_MIN;
+    
+    cout<<endl<<"maximum value: "<<findMax(root);
+    delete root;
+    return 0;
+}
