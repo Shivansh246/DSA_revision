c893dd635afbb294d76521cb715340d6e9e183b8
Shivansh246 <285635402+Shivansh246@users.noreply.github.com>
2026-07-27 19:53:01 +0530

    types of graph

---
+++ 28_Graph_Traversal_algo_DFS_BFS/types_of_Graph.cpp
@@ -0,0 +1,11 @@
+/*
+Directed Graph =>
+no bidirectional edges, if one node points to other that does not mean that the other node will point to previous node
+
+Weighted Graph =>
+In unweighted graph all the edges are equivalent or all carries the weight of 1 and if there is no edge the weight is 0
+in weighted graph the edge will also carry the weight i.e. while updating the adjacency matrix we will store matrix[i][j] = w,
+where w is the weight of the edge,
+weight can have the analogy of cost of travel through a route , if the weight is more through a route then we can say that the has
+more weight
+*/
